325f63a4c4e965292873a705a922f76f94135a0d
Gordon <weiguo_1987@163.com>
2016-02-18 16:31:29 +0800

    把整个aauto项目加入git中，包括aauto的开发环境、编译环境、实例等

---
+++ extensions/wizard/project/web/webform/view/js/main.js
@@ -0,0 +1,29 @@
+﻿require.config({
+	paths: {
+		jquery: 'jQuery/jquery.min' //可在这里自定义库文件的实际路径,省略'.js'文件后缀名 
+	}
+});
+ 
+require(
+	["jquery","aardio"], //异步导入JS库文件
+	function($,aardio) { //请求的库加载完成后触发此函数 
+		$(document).ready( 
+			function(){ //在网页完成加载后激活函数 
+				//添加事件
+				$("#btnSysPath").on("click",
+					function(){  
+						aardio.getSysPath({
+							onEnd:function(path){
+								$("#txtSysPath").html( path );
+							}
+						});
+					} 
+				);
+
+				//动画效果
+				$("#form_main").hide().fadeIn("slow");
+			}
+		);
+	}
+);
+ 
